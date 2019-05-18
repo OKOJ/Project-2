d3e98d83a923346fa3b5eda7641855297d551107
marco <marcobjohns@gmail.com>
2019-05-18 13:45:09 -0600

    Got the google maps to display on main page.  Also hard coded locations to display on the maps.

--- public/js/signup.js
+++ public/js/signup.js
@@ -21,3 +21,18 @@ $("#user-sign-up").on("submit", function(e) {
       alert(err.responseText);
     });
 });
+
+function initMap() {
+
+  var uluru = { lat: 40.7608, lng: -111.8910 };
+  var location1 = { lat: 40.3960304, lng: -111.9246005 };
+  var location2 = { lat: 40.8495378, lng: -111.7252947 };
+  var location3 = { lat: 40.5760371, lng: -111.8086808 };
+  var map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 9, center: uluru });
+  var Marker = new google.maps.Marker({ position: uluru, map: map });
+  var Marker2 = new google.maps.Marker({ position: location1, map: map });
+  var Marker3 = new google.maps.Marker({ position: location2, map: map }); 
+  var Marker4 = new google.maps.Marker({ position: location3, map: map }); 
+  
+} 
